b12c49d6c06c4e57e3ae7f27d17c51da47479a82
Nancy Li <nancyli1996@gmail.com>
2016-09-10 11:09:21 -0400

    fixed connect thing

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 var User = require("./models/user.js");
 var Ride = require("./models/rides.js");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myappdatabase');
--- models/rides.js
+++ models/rides.js
@@ -1,7 +1,6 @@
 var User = require('./user.js');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myappdatabase');
 
 var Schema = mongoose.Schema;
 var rideSchema = new Schema({
