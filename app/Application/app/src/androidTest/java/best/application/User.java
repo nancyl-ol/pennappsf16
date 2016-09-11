9d21c27916b14c1191af30e80626815f55dfcec7
jcoc611 <jcoc611@gmail.com>
2016-09-10 21:44:28 -0400

    Added basic login auth

--- app/Application/app/src/androidTest/java/best/application/User.java
+++ app/Application/app/src/androidTest/java/best/application/User.java
@@ -2,6 +2,7 @@ package best.application;
 
 import android.util.Pair;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.List;
  * Created by jcoc611 on 2016-09-10.
  */
 public class User {
+    String id;
     String username;
     String name;
     Integer age;
@@ -18,8 +20,22 @@ public class User {
     String occupation;
     String number;
 
-    public User(String usr){
-        this.username = usr;
+    public User(
+            String id,
+            String username,
+            String name,
+            Integer age,
+            String gender,
+            String occupation,
+            String number
+    ){
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+        this.number = number;
     }
 
     public static User fromAuth(String username, String password){
@@ -28,11 +44,30 @@ public class User {
         List<Pair<String, ?>> authData = new ArrayList<Pair<String, ?>>();
         authData.add(new Pair<String, String>("a", "b"));
 
-        JSONObject userData = api.get("api/login", authData);
-        if(userData.has("error")){
+        JSONObject response = api.get("api/login", authData);
+
+        if(response.has("error")){
             throw new Exception("Damn daniel");
         }
 
-        return new User(username);
+        JSONObject userData;
+        User newUser;
+
+        try {
+
+            userData = response.getJSONObject("user");
+            newUser = new User(
+                    userData.getString("_id"),
+                    userData.getString("name"),
+                    userData.getString("username"),
+                    userData.getInt("age"),
+                    userData.getString("gender"),
+                    userData.getString("occupation"),
+                    userData.getString("number")
+            );
+        }catch(JSONException e){
+            return null;
+        }
+        return newUser;
     }
 }
